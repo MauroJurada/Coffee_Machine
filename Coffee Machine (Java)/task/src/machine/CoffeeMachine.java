8a19804e9094bdbe5e51fb7940f65f0581ad8b17
Mauro Jurada <37582356+MauroJurada@users.noreply.github.com>
2019-07-22 22:52:45 +0200

    stage #2

--- Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
+++ Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
@@ -1,7 +1,25 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Starting to make a coffee\n" +
+                "Grinding coffee beans\n" +
+                "Boiling water\n" +
+                "Mixing boiled water with crushed coffee beans\n" +
+                "Pouring coffee into the cup\n" +
+                "Pouring some milk into the cup\n" +
+                "Coffee is ready!");
+        System.out.println("Write how many cups of coffee you will need:");
+
+        int x = sc.nextInt();
+
+        System.out.println("For " + x + "cups of coffee you will need:\n" +
+                200*x + "ml of water\n" + 50*x + "ml of milk\n" + 15*x + "g of coffee beans");
+
     }
 }
--- Coffee Machine (Java)/task/test/CoffeeMachineTest.java
+++ Coffee Machine (Java)/task/test/CoffeeMachineTest.java
@@ -6,32 +6,140 @@ import org.hyperskill.hstest.v5.testcase.TestCase;
 import java.util.List;
 
 
-public class CoffeeMachineTest extends BaseStageTest<String> {
+class TestClue {
+    boolean cond;
+    int num;
+    boolean showTests;
+    TestClue(boolean c, int n, boolean showTests) {
+        cond = c;
+        num = n;
+        this.showTests = showTests;
+    }
+}
+
+public class CoffeeMachineTest extends BaseStageTest<TestClue> {
 
     public CoffeeMachineTest() {
         super(CoffeeMachine.class);
     }
 
     @Override
-    public List<TestCase<String>> generate() {
+    public List<TestCase<TestClue>> generate() {
         return List.of(
-            new TestCase<String>()
-                .setInput("")
-                .setAttach("Starting to make a coffee\n" +
-                    "Grinding coffee beans\n" +
-                    "Boiling water\n" +
-                    "Mixing boiled water with crushed coffee beans\n" +
-                    "Pouring coffee into the cup\n" +
-                    "Pouring some milk into the cup\n" +
-                    "Coffee is ready!")
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 0, true))
+                .setInput("300\n65\n111\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 2, true))
+                .setInput("600\n153\n100\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 2, true))
+                .setInput("1400\n150\n100\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 2, true))
+                .setInput("1400\n1500\n45\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(false, 2, true))
+                .setInput("599\n250\n200\n10"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 867, true))
+                .setInput( "345640\n43423\n23234\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(false, 1548, true))
+                .setInput("345640\n434230\n23234\n19246"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(false, 172, true))
+                .setInput( "34564\n43423\n23234\n19246"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 0, false))
+                .setInput("399\n112\n111\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 3, false))
+                .setInput("2400\n249\n100\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 1, false))
+                .setInput("1400\n1500\n44\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(false, 2, false))
+                .setInput("500\n250\n200\n10"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 171, false))
+                .setInput("34564\n43423\n23234\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(true, 1547, false))
+                .setInput("345640\n434230\n23234\n1"),
+
+            new TestCase<TestClue>()
+                .setAttach(new TestClue(false, 868, false))
+                .setInput("345640\n43423\n23234\n19246")
+
         );
     }
 
     @Override
-    public CheckResult check(String reply, String clue) {
-        boolean isCorrect = reply.trim().equals(clue.trim());
-        return new CheckResult(isCorrect,
-            "You should make coffee exactly " +
-                "like in the example");
+    public CheckResult check(String reply, TestClue clue) {
+        String[] lines = reply.trim().split(":");
+        String userOutput = lines[lines.length - 1].trim();
+        String loweredOutput = userOutput.toLowerCase();
+        boolean ans = clue.cond;
+        int amount = clue.num;
+        if (ans && loweredOutput.contains("yes")) {
+            if (amount > 0) {
+                boolean isCorrect = loweredOutput.contains(Integer.toString(amount));
+                if (isCorrect) {
+                    return CheckResult.TRUE;
+                } else {
+
+                    String rightOutput =
+                        "Yes, I can make that amount of coffee" +
+                            " (and even " + amount + " more than that)";
+
+                    if (clue.showTests) {
+                        return new CheckResult(false,
+                            "Your output:\n" +
+                                userOutput +
+                                "\nRight output:\n" +
+                                rightOutput);
+                    } else {
+                        return CheckResult.FALSE;
+                    }
+                }
+            }
+            return CheckResult.TRUE;
+        } else {
+            boolean cond1 = loweredOutput.contains("no");
+            boolean cond2 = loweredOutput.contains(Integer.toString(amount));
+
+            if (cond1 && cond2) {
+                return CheckResult.TRUE;
+            } else {
+
+                String rightOutput = "No, I can make only " +
+                    amount +" cup(s) of coffee";
+
+                if (clue.showTests) {
+                    return new CheckResult(false,
+                        "Your output:\n" +
+                        userOutput +
+                        "\nRight output:\n" +
+                        rightOutput);
+                } else {
+                    return CheckResult.FALSE;
+                }
+            }
+        }
     }
 }
